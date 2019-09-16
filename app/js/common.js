3c21a050114c666948524e9fe963fc6b6356790c
BetskoDenis <47107535+BetskoDenis@users.noreply.github.com>
2019-09-16 21:00:38 +0300

    001

---
+++ app/js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $(".arr").on('click', function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+
+})
