2973514a8dcbc2b7a95d341dfa2b07468d1e4217
BetskoDenis <47107535+BetskoDenis@users.noreply.github.com>
2019-09-18 16:56:51 +0300

    004

--- app/js/common.js
+++ app/js/common.js
@@ -7,4 +7,35 @@ $(document).ready(function () {
         $('body,html').animate({scrollTop: top}, 1000);
     });
 
+    $('.carousel__wrapper').owlCarousel({
+        items: 1,
+        loop: true,
+        dots: false,
+        nav: true,
+        smartSpeed: 1300
+
+    });
+    $('.our_works__wrapper').owlCarousel({
+        center: true,
+        margin:10,
+        loop: true,
+        dots: false,
+        autoplay: false,
+        nav: true,
+        smartSpeed: 1300,
+        responsive:{
+            0:{
+                items:1
+            },
+            680:{
+                items:2
+            },
+            1000:{
+                items:3
+            },
+
+        }
+
+    });
+
 })
